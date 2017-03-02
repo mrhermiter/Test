38109b1c9fd6cc28c5d086b83bac1fcae54dd043
dmk <mrhermiter@gmail.com>
2017-03-02 15:42:23 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,109 @@
+/**
+ * Created by dmk on 27.02.17.
+ */
+const port = 3000;
+const imgScreen="screen.png";
+
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io").listen(server);
+const screenshot = require('desktop-screenshot');
+const fs = require("fs");
+
+var users = [];
+var connections = [];
+
+server.listen(port);
+console.log("Running...");
+
+app.use(express.static(__dirname + "/public"));
+
+const chat = io.of("/chat").on("connection", (socket) => {
+
+    connections.push(socket);
+    console.log("New connection socket: " + socket.id);
+    console.log("Current connections: " + connections.length);
+
+    socket.on("disconnect", (data) => {
+
+        if (socket.userName)
+            users.splice(users.indexOf(socket.userName), 1);
+        connections.splice(connections.indexOf(socket), 1);
+        updateUsers();
+        console.log("Disconnected socket: " + socket.id);
+        console.log("Current connections: " + connections.length);
+    });
+
+    socket.on("setUserName", (data, callback) => {
+
+        socket.userName = data;
+        users.push(data);
+        callback(data);
+        updateUsers();
+        console.log(users);
+    });
+
+    socket.on("sendMessage", (data) => {
+
+        chat.emit("newMessage", {msg: data, usr: socket.userName});
+    });
+
+    function updateUsers() {
+
+        chat.emit("updateUsers", users);
+    }
+});
+
+const screen = io.of("/screen").on("connection", (socket) => {
+    console.log("New screen connection: " + socket.id);
+
+    setInterval(function() {
+        sendScreen();
+    }, 100);
+    //sendScreen();
+    /*socket.on("getSreen",()=>{
+        sendScreen();
+    });*/
+    function sendScreen() {
+        screenshot(imgScreen, (error, complete) => {
+            if(error)
+                console.log("Screenshot failed", error);
+            else {
+                fs.createReadStream(imgScreen).on("data",(chunk)=>{
+                    screen.emit("newScreen",chunk);
+                    console.log(chunk);
+                }).on("end",()=>{
+                    screen.emit("endNewScreen",true);
+                    console.log("End Send");
+                });
+
+
+            }
+        });
+    }
+});
+
+
+
+
+
+
+ /*app.get("/", (req, res) => {
+ res.write("ololo");
+ console.log("write ololo");
+ });
+*/
+
+
+/*
+ const screenshot = require('desktop-screenshot');
+ screenshot(imgScreen, (error, complete) => {
+ if(error)
+ console.log("Screenshot failed", error);
+ else {
+ fs.createReadStream(imgScreen).pipe(res);
+ console.log(complete);
+
+ }
+ })*/
---
+++ public/client.js
@@ -0,0 +1,110 @@
+/**
+ * Created by dmk on 28.02.17.
+ */
+$(function () {
+
+    const chat = io.connect("http://localhost:3000/chat");
+    const screen=io.connect("http://localhost:3000/screen");
+    $("#userNameForm").submit((e) => {
+
+        e.preventDefault();
+        if ($("#userName").val() != "")
+            chat.emit("setUserName", $("#userName").val(), (data) => {
+
+                if (data) {
+
+                    $("#userNameForm").hide();
+                    $("#chatPanel").show();
+                    $("#message").val("").focus();
+                    $("#userName").val("");
+                }
+                else {
+
+                    alert("Please, try again!");
+                }
+            });
+        else
+            alert("Please, enter your name.");
+    });
+
+    chat.on("updateUsers", (data) => {
+
+        updateChatUsers(data);
+    });
+    $("#message").keydown((e) => {
+
+        if (e.ctrlKey && e.keyCode == 13) {
+
+            $("#sendMessageForm").submit();
+            return false;
+        }
+    });
+    $("#sendMessageForm").submit((e) => {
+
+        e.preventDefault();
+        let message = nl2br($("#message").val());
+        if (message === "")
+            return false;
+        chat.emit("sendMessage", message);
+        $("#message").val("").focus();
+    });
+
+    chat.on("newMessage", (data) => {
+
+        showNewMessage(data);
+    });
+
+
+    $("#btnTest").click((e)=>{
+
+        e.preventDefault();
+        screen.emit("getSreen","");
+        return false;
+    });
+    var view=[];
+    screen.on("newScreen",(data)=>{
+
+        view.push(new Uint8Array(data));
+        console.log(view);
+    });
+
+
+/*    var example = document.getElementById("image"),
+        ctx       = example.getContext('2d'), // Контекст
+        pic       = new Image();              // "Создаём" изображение
+      // Источник изображения, позаимствовано на хабре
+    pic.onload = function() {    // Событие onLoad, ждём момента пока загрузится изображение
+        ctx.drawImage(pic, 0, 0);  // Рисуем изображение от точки с координатами 0, 0
+    }*/
+
+    screen.on("endNewScreen",(data)=>{
+        //console.log(data);
+        var blob = new Blob(view, {type: "image/png"});
+        var a=URL.createObjectURL(blob);
+
+        $("#image").attr("src",a);
+        view=[];
+    });
+});
+
+
+function updateChatUsers(data) {
+
+    var listUsers = "";
+    for (let i = 0; i < data.length; i++) {
+
+        listUsers += '<li class="list-group-item">' + data[i] + '</li>';
+    }
+    $("#chatUsers").html(listUsers);
+}
+
+function showNewMessage(data) {
+
+    $("#chatArea").append('<div class="well"><strong>' + data.usr + '</strong>: ' + data.msg + '</div>');
+    document.getElementById("chatArea").scrollTop = document.getElementById("chatArea").scrollHeight;
+}
+
+function nl2br(s) {
+    s = s.split("\u000A").join("<br />\u000A");
+    return s;
+}
